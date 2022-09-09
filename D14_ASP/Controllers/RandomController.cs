8624e69072780840b20fc4558080b148549ed51c
ChristianKastberg <56236299+ChristianKastberg@users.noreply.github.com>
2022-09-09 13:47:15 +0200

    Add files via upload

---
+++ D14_ASP/Controllers/RandomController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace D14_ASP.Controllers
+{
+    public class RandomController : Controller
+    {
+        // GET: Random
+        public ActionResult Index()
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(1, 7);
+
+            ViewBag.i = i;
+            return View();
+        }
+    }
+}
---
+++ D14_ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="D14_ASP.MvcApplication" Language="C#" %>
