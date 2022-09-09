b527c7aef3dac637f0f5aa458ad140f6591534dd
ChristianKastberg <56236299+ChristianKastberg@users.noreply.github.com>
2022-09-09 13:35:06 +0200

    Add files via upload

---
+++ D6_WPF_opg/opg2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace opg2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) // Wrong 
+        {
+            var x = label1.Content;
+            var y = label2.Content;
+            x = label2.Content;
+            y = label1.Content;
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var x = label3.Content;
+            var y = label4.Content;
+            x = label4.Content;
+            y = label3.Content;
+        }
+    }
+}
